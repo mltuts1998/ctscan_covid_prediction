7198353e6c154632e7d1a08b8d511eb1ef951d1a
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-03-01 17:10:48 +0530

    Added the model with images

---
+++ predit.py
@@ -0,0 +1,475 @@
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+from PIL import Image
+import matplotlib.pyplot as plt
+from torch.optim.lr_scheduler import StepLR
+from PIL import ImageFile
+from torch.utils.tensorboard import SummaryWriter
+from datetime import datetime
+import pandas as pd
+import random 
+from shutil import copyfile
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+import torchxrayvision as xrv
+import torch
+import torchvision
+from torchvision import transforms, utils
+from torch.utils.data import Dataset, DataLoader
+import torch.optim as optim
+from torch.utils.data import Dataset
+import os
+import torch.nn as nn
+import numpy as np
+from sklearn.metrics import roc_auc_score
+import re
+import albumentations as albu
+from albumentations.pytorch import ToTensor
+from catalyst.data import Augmentor
+from skimage.io import imread, imsave
+import skimage
+
+torch.cuda.empty_cache()
+
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+train_transformer = transforms.Compose([
+    transforms.Resize(256),
+    transforms.RandomResizedCrop((224),scale=(0.5,1.0)),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    normalize
+])
+
+val_transformer = transforms.Compose([
+    transforms.Resize(224),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    normalize
+])
+
+
+batchsize=10
+def read_txt(txt_path):
+    with open(txt_path) as f:
+        lines = f.readlines()
+    txt_data = [line.strip() for line in lines]
+    return txt_data
+
+class CovidCTDataset(Dataset):
+    def __init__(self, root_dir, txt_COVID, txt_NonCOVID, transform=None):
+        """
+        Args:
+            txt_path (string): Path to the txt file with annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        File structure:
+        - root_dir
+            - CT_COVID
+                - img1.png
+                - img2.png
+                - ......
+            - CT_NonCOVID
+                - img1.png
+                - img2.png
+                - ......
+        """
+        self.root_dir = root_dir
+        self.txt_path = [txt_COVID,txt_NonCOVID]
+        self.classes = ['COVID', 'NonCOVID']
+        self.num_cls = len(self.classes)
+        self.img_list = []
+        for c in range(self.num_cls):
+            cls_list = [[os.path.join(self.root_dir,self.classes[c],item), c] for item in read_txt(self.txt_path[c])]
+            self.img_list += cls_list
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.img_list)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img_path = self.img_list[idx][0]
+        image = Image.open(img_path).convert('RGB')
+
+        if self.transform:
+            image = self.transform(image)
+        sample = {'img': image,
+                  'label': int(self.img_list[idx][1])}
+        return sample
+
+
+if __name__ == '__main__':
+    trainset = CovidCTDataset(root_dir='images',
+                              txt_COVID='data/COVID/train.txt',
+                              txt_NonCOVID='data/NonCOVID/train.txt',
+                              transform= train_transformer)
+    valset = CovidCTDataset(root_dir='images',
+                              txt_COVID='data/COVID/val.txt',
+                              txt_NonCOVID='data/NonCOVID/val.txt',
+                              transform= val_transformer)
+    testset = CovidCTDataset(root_dir='images',
+                              txt_COVID='data/COVID/test.txt',
+                              txt_NonCOVID='data/NonCOVID/test.txt',
+                              transform= val_transformer)
+    print(trainset.__len__())
+    print(valset.__len__())
+    print(testset.__len__())
+
+    train_loader = DataLoader(trainset, batch_size=batchsize, drop_last=False, shuffle=True)
+    val_loader = DataLoader(valset, batch_size=batchsize, drop_last=False, shuffle=False)
+    test_loader = DataLoader(testset, batch_size=batchsize, drop_last=False, shuffle=False)
+    
+
+alpha = None
+device = 'cpu'
+def train(optimizer, epoch):
+    
+    model.train()
+    
+    train_loss = 0
+    train_correct = 0
+    
+    for batch_index, batch_samples in enumerate(train_loader):
+        
+        # move data to device
+        data, target = batch_samples['img'].to(device), batch_samples['label'].to(device)
+        
+        optimizer.zero_grad()
+        output = model(data)
+        
+        criteria = nn.CrossEntropyLoss()
+        loss = criteria(output, target.long())
+        train_loss += criteria(output, target.long())
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        pred = output.argmax(dim=1, keepdim=True)
+        train_correct += pred.eq(target.long().view_as(pred)).sum().item()
+    
+        # Display progress and write to tensorboard
+        if batch_index % bs == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tTrain Loss: {:.6f}'.format(
+                epoch, batch_index, len(train_loader),
+                100.0 * batch_index / len(train_loader), loss.item()/ bs))
+    
+    print('\nTrain set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        train_loss/len(train_loader.dataset), train_correct, len(train_loader.dataset),
+        100.0 * train_correct / len(train_loader.dataset)))
+    f = open('model_result/{}.txt'.format(modelname), 'a+')
+    f.write('\nTrain set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        train_loss/len(train_loader.dataset), train_correct, len(train_loader.dataset),
+        100.0 * train_correct / len(train_loader.dataset)))
+    f.write('\n')
+    f.close()
+
+def val(epoch):
+    
+    model.eval()
+    test_loss = 0
+    correct = 0
+    results = []
+    
+    TP = 0
+    TN = 0
+    FN = 0
+    FP = 0
+    
+    
+    criteria = nn.CrossEntropyLoss()
+    # Don't update model
+    with torch.no_grad():
+        tpr_list = []
+        fpr_list = []
+        
+        predlist=[]
+        scorelist=[]
+        targetlist=[]
+        # Predict
+        for batch_index, batch_samples in enumerate(val_loader):
+            data, target = batch_samples['img'].to(device), batch_samples['label'].to(device)
+            output = model(data)
+            
+            test_loss += criteria(output, target.long())
+            score = F.softmax(output, dim=1)
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.long().view_as(pred)).sum().item()
+            
+            targetcpu=target.long().cpu().numpy()
+            predlist=np.append(predlist, pred.cpu().numpy())
+            scorelist=np.append(scorelist, score.cpu().numpy()[:,1])
+            targetlist=np.append(targetlist,targetcpu)
+           
+          
+    return targetlist, scorelist, predlist
+
+
+def test(epoch):
+    
+    model.eval()
+    test_loss = 0
+    correct = 0
+    results = []
+    
+    TP = 0
+    TN = 0
+    FN = 0
+    FP = 0
+    
+    
+    criteria = nn.CrossEntropyLoss()
+    # Don't update model
+    with torch.no_grad():
+        tpr_list = []
+        fpr_list = []
+        
+        predlist=[]
+        scorelist=[]
+        targetlist=[]
+        # Predict
+        for batch_index, batch_samples in enumerate(test_loader):
+            data, target = batch_samples['img'].to(device), batch_samples['label'].to(device)
+            output = model(data)
+            
+            test_loss += criteria(output, target.long())
+            score = F.softmax(output, dim=1)
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.long().view_as(pred)).sum().item()
+            targetcpu=target.long().cpu().numpy()
+            predlist=np.append(predlist, pred.cpu().numpy())
+            scorelist=np.append(scorelist, score.cpu().numpy()[:,1])
+            targetlist=np.append(targetlist,targetcpu)
+           
+    return targetlist, scorelist, predlist
+
+
+class DenseNetModel(nn.Module):
+
+    def __init__(self):
+        """
+        Pass in parsed HyperOptArgumentParser to the model
+        :param hparams:
+        """
+        super(DenseNetModel, self).__init__()
+
+        self.dense_net = xrv.models.DenseNet(num_classes=2)
+        self.criterion = nn.CrossEntropyLoss()
+
+    def forward(self, x):
+        logits = self.dense_net(x)
+        return logits
+    
+model = DenseNetModel().cpu()
+modelname = 'DenseNet_medical'
+
+
+### SimpleCNN
+class SimpleCNN(torch.nn.Module):
+    def __init__(self):
+        super(SimpleCNN, self).__init__() # b, 3, 32, 32
+        layer1 = torch.nn.Sequential() 
+        layer1.add_module('conv1', torch.nn.Conv2d(3, 32, 3, 1, padding=1))
+ 
+        layer1.add_module('relu1', torch.nn.ReLU(True)) 
+        layer1.add_module('pool1', torch.nn.MaxPool2d(2, 2)) 
+        self.layer1 = layer1
+        layer4 = torch.nn.Sequential()
+        layer4.add_module('fc1', torch.nn.Linear(401408, 2))       
+        self.layer4 = layer4
+ 
+    def forward(self, x):
+        conv1 = self.layer1(x)
+        fc_input = conv1.view(conv1.size(0), -1)
+        fc_out = self.layer4(fc_input)
+ 
+model = SimpleCNN().cpu()
+modelname = 'SimpleCNN'
+
+
+### ResNet18
+import torchvision.models as models
+model = models.resnet18(pretrained=True).cpu()
+modelname = 'ResNet18'
+
+
+### Dense121
+import torchvision.models as models
+model = models.densenet121(pretrained=True).cpu()
+modelname = 'Dense121'
+
+
+### Dense169
+import torchvision.models as models
+model = models.densenet169(pretrained=True).cpu()
+modelname = 'Dense169'
+
+
+### ResNet50
+import torchvision.models as models
+model = models.resnet50(pretrained=True).cpu()
+modelname = 'ResNet50'
+
+
+### VGGNet
+import torchvision.models as models
+model = models.vgg16(pretrained=True)
+model = model.cpu()
+modelname = 'vgg16'
+
+
+### efficientNet
+from efficientnet_pytorch import EfficientNet
+model = EfficientNet.from_pretrained('efficientnet-b0', num_classes=2)
+model = model.cpu()
+modelname = 'efficientNet-b0'
+
+
+# train
+bs = 10
+votenum = 10
+import warnings
+warnings.filterwarnings('ignore')
+
+r_list = []
+p_list = []
+acc_list = []
+AUC_list = []
+vote_pred = np.zeros(valset.__len__())
+vote_score = np.zeros(valset.__len__())
+
+optimizer = optim.Adam(model.parameters(), lr=0.0001)
+scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10)
+
+total_epoch = 1
+for epoch in range(1, total_epoch+1):
+    train(optimizer, epoch)
+    
+    targetlist, scorelist, predlist = val(epoch)
+    print('target',targetlist)
+    print('score',scorelist)
+    print('predict',predlist)
+    vote_pred = vote_pred + predlist 
+    vote_score = vote_score + scorelist 
+
+    if epoch % votenum == 0:
+        
+        # major vote
+        vote_pred[vote_pred <= (votenum/2)] = 0
+        vote_pred[vote_pred > (votenum/2)] = 1
+        vote_score = vote_score/votenum
+        
+        print('vote_pred', vote_pred)
+        print('targetlist', targetlist)
+        TP = ((vote_pred == 1) & (targetlist == 1)).sum()
+        TN = ((vote_pred == 0) & (targetlist == 0)).sum()
+        FN = ((vote_pred == 0) & (targetlist == 1)).sum()
+        FP = ((vote_pred == 1) & (targetlist == 0)).sum()
+        
+        
+        print('TP=',TP,'TN=',TN,'FN=',FN,'FP=',FP)
+        print('TP+FP',TP+FP)
+        p = TP / (TP + FP)
+        print('precision',p)
+        p = TP / (TP + FP)
+        r = TP / (TP + FN)
+        print('recall',r)
+        F1 = 2 * r * p / (r + p)
+        acc = (TP + TN) / (TP + TN + FP + FN)
+        print('F1',F1)
+        print('acc',acc)
+        AUC = roc_auc_score(targetlist, vote_score)
+        print('AUCp', roc_auc_score(targetlist, vote_pred))
+        print('AUC', AUC)
+        
+        torch.save(model.state_dict(), "model_backup/{}.pt".format(modelname))  
+        
+        vote_pred = np.zeros(valset.__len__())
+        vote_score = np.zeros(valset.__len__())
+        print('\n The epoch is {}, average recall: {:.4f}, average precision: {:.4f},average F1: {:.4f}, average accuracy: {:.4f}, average AUC: {:.4f}'.format(
+        epoch, r, p, F1, acc, AUC))
+
+        f = open('model_result/{}.txt'.format(modelname), 'a+')
+        f.write('\n The epoch is {}, average recall: {:.4f}, average precision: {:.4f},average F1: {:.4f}, average accuracy: {:.4f}, average AUC: {:.4f}'.format(
+        epoch, r, p, F1, acc, AUC))
+        f.close()
+
+# test
+bs = 10
+import warnings
+warnings.filterwarnings('ignore')
+
+r_list = []
+p_list = []
+acc_list = []
+AUC_list = []
+vote_pred = np.zeros(testset.__len__())
+vote_score = np.zeros(testset.__len__())
+
+optimizer = optim.Adam(model.parameters(), lr=0.0001)
+scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10)
+scheduler = StepLR(optimizer, step_size=1)
+
+total_epoch = 10
+for epoch in range(1, total_epoch+1):
+    
+    targetlist, scorelist, predlist = test(epoch)
+    vote_pred = vote_pred + predlist 
+    vote_score = vote_score + scorelist 
+    
+    TP = ((predlist == 1) & (targetlist == 1)).sum()
+    TN = ((predlist == 0) & (targetlist == 0)).sum()
+    FN = ((predlist == 0) & (targetlist == 1)).sum()
+    FP = ((predlist == 1) & (targetlist == 0)).sum()
+
+    print('TP=',TP,'TN=',TN,'FN=',FN,'FP=',FP)
+    print('TP+FP',TP+FP)
+    p = TP / (TP + FP)
+    print('precision',p)
+    p = TP / (TP + FP)
+    r = TP / (TP + FN)
+    print('recall',r)
+    F1 = 2 * r * p / (r + p)
+    acc = (TP + TN) / (TP + TN + FP + FN)
+    print('F1',F1)
+    print('acc',acc)
+    AUC = roc_auc_score(targetlist, vote_score)
+    print('AUC', AUC)
+
+    if epoch % votenum == 0:
+        
+        # major vote
+        vote_pred[vote_pred <= (votenum/2)] = 0
+        vote_pred[vote_pred > (votenum/2)] = 1
+        TP = ((vote_pred == 1) & (targetlist == 1)).sum()
+        TN = ((vote_pred == 0) & (targetlist == 0)).sum()
+        FN = ((vote_pred == 0) & (targetlist == 1)).sum()
+        FP = ((vote_pred == 1) & (targetlist == 0)).sum()
+        
+        print('TP=',TP,'TN=',TN,'FN=',FN,'FP=',FP)
+        print('TP+FP',TP+FP)
+        p = TP / (TP + FP)
+        print('precision',p)
+        p = TP / (TP + FP)
+        r = TP / (TP + FN)
+        print('recall',r)
+        F1 = 2 * r * p / (r + p)
+        acc = (TP + TN) / (TP + TN + FP + FN)
+        print('F1',F1)
+        print('acc',acc)
+        AUC = roc_auc_score(targetlist, vote_score)
+        print('AUC', AUC)
+        
+        vote_pred = np.zeros((1,testset.__len__()))
+        vote_score = np.zeros(testset.__len__())
+        print('vote_pred',vote_pred)
+        print('\n The epoch is {}, average recall: {:.4f}, average precision: {:.4f},average F1: {:.4f}, average accuracy: {:.4f}, average AUC: {:.4f}'.format(
+        epoch, r, p, F1, acc, AUC))
+
+        f = open(f'model_result/test_{modelname}.txt', 'a+')
+        f.write('\n The epoch is {}, average recall: {:.4f}, average precision: {:.4f},average F1: {:.4f}, average accuracy: {:.4f}, average AUC: {:.4f}'.format(
+        epoch, r, p, F1, acc, AUC))
+        f.close()
